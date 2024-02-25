5972a2e9a2093f9023329a81ade94f63af05c62e
ssttkkl <huang.wen.long@hotmail.com>
2024-02-25 17:27:07 +0800

    Commit

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,31 +1,24 @@
+@file:OptIn(ExperimentalResourceApi::class)
+
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import kotlinproject.composeapp.generated.resources.Res
-import kotlinproject.composeapp.generated.resources.num_text
+import kotlinproject.composeapp.generated.resources.icon_arrow_outward
+import kotlinproject.composeapp.generated.resources.icon_drag_handle
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.imageResource
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        var count by remember { mutableStateOf(0) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(stringResource(Res.string.num_text, count))
-            Text(count.toString())
-            Button(onClick = { count++ }) {
-                Text("Click me!")
-            }
+        Column(Modifier.fillMaxWidth()) {
+            Icon(imageResource(Res.drawable.icon_drag_handle), "")
+            Icon(imageResource(Res.drawable.icon_arrow_outward), "")
         }
     }
 }
